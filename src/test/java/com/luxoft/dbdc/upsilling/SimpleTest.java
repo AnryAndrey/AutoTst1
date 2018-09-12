8a3d334bbfbc0f663f4ed8da063bfd152b20bc57
ptc-hp-2 <ptc-hp-2@ptc.local>
2018-09-12 11:35:01 +0300

    test1

---
+++ src/test/java/com/luxoft/dbdc/upsilling/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.luxoft.dbdc.upsilling;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void alwayspass(){
+        Assertions.assertTrue(true);
+    }
+}
