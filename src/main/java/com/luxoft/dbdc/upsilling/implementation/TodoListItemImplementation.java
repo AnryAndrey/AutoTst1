9c43825b757fb8b98bc533ccc0936db4fdd4cc4c
ptc-hp-2 <ptc-hp-2@ptc.local>
2018-09-12 12:12:28 +0300

    5

--- src/main/java/com/luxoft/dbdc/upsilling/implementation/TodoListItemImplementation.java
+++ src/main/java/com/luxoft/dbdc/upsilling/implementation/TodoListItemImplementation.java
@@ -5,8 +5,22 @@ import com.luxoft.dbdc.upsilling.interfaces.Togglable;
 
 public class TodoListItemImplementation implements Togglable {
 
+    private boolean toggled ;
+
+    private String title;
+    public TodoListItemImplementation(String title){
+        this.title = title;
+        toggled = false;
+    }
+
     @Override
     public void toggle(){
-
     }
+
+    public boolean isToggled(){
+        return toggled;
+    }
+
+
+
 }
